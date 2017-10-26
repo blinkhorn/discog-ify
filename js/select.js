a52f2850f976dc9539ecef6529277f803d4486ef
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2017-10-26 15:26:12 -0400

    programmed functions for adding label releases

--- js/select.js
+++ js/select.js
@@ -4,7 +4,11 @@
 //                                  *
 //***********************************/
 
-const spotify_token;
+let spotify_token = null;
+let globalArtists = [];
+let globalLabels = [];
+let totalReleases = 0;
+let totalLabels = 0;
 
 /************************************
 //                                  *
@@ -12,13 +16,27 @@ const spotify_token;
 //                                  *
 //***********************************/
 class Release {
-  constructor(title, artistName, year) {
+  constructor(title, artistName, year, label) {
     this.title = title;
     this.artistName = artistName;
     this.year = year;
+    this.label;
   }
 }
 
+class Artist {
+  constructor(name, releases) {
+    this.name = name;
+    this.releases = releases;
+  }
+}
+
+class Label {
+  constructor(name, releases) {
+    this.name = name;
+    this.releases = releases;
+  }
+}
 /************************************
 //                                  *
 //      Function Expressions        *
@@ -26,7 +44,7 @@ class Release {
 //***********************************/
 
 // Gets parameters from the URL
-const urlParams = function getURLParams() {
+function getURLParams() {
   let urlParams = {};
   let result;
   let regex = /([^&;=]+)=?([^&;]*)/g; //isolate sections of params separated by '='
@@ -35,7 +53,7 @@ const urlParams = function getURLParams() {
     urlParams[result[1]] = decodeURIComponent(result[2]);
   }
   return urlParams;
-}
+};
 
 //prevents duplicate artists from being in the global array. If the array contains
 //the artist's name, it returns the position in the array; otherwise it returns null
@@ -46,49 +64,61 @@ const containsName = function artistsContainsName(name) {
         }
     }
     return null;
-}
+};
+
+//prevents duplicate labels from being in the global array. If the array contains
+//the label's name, it returns the position in the array; otherwise it returns null
+const containsLabel = function labelsContainsName(name) {
+    for (var i = 0; i < globalLabels.length; i += 1) {
+        if (globalLabels[i].name === name) {
+            return i;
+        }
+    }
+    return null;
+};
 
 //takes the result returned from accessing all label releases from discogs and
 //adds them to the global array (duplicate releases aren't allowed)
 const aLReleases = function addLabelReleases(discogsResult) {
 
-    $.each(discogsResult.releases, (pos, release) => {
+    $.each(discogsResult.releases, (release) => {
 
-        let releaseTitle = release.basic_information.title;
-        let releaseYear = release.basic_information.year;
-        let releaseArtists = release.basic_information.artists;
+        let releaseTitle = release.title;
+        let releaseYear = release.year;
+        let releaseArtists = release.artists;
         let releaseArtistName = releaseArtists[0].name;
+        let releaseLabelName = release.label;
 
-        //Some artists on Discogs have a number in closing round parenthesis behind their name. We don't want these.
+        //Some artists/labels on Discogs have a number in closing round
+        //parenthesis behind their name — we prevent these here
         let splitName = releaseArtistName.split(/([(]\d+[)].*)/);
         let artistName = splitName[0];
+        let splitLabel = releaseLabelName.split(/([(]\d+[)].*)/);
+        let releaseLabel = splitLabel[0];
 
-        let theRelease = new Release(releaseTitle, artistName, releaseYear);
+        let theRelease = new Release(releaseTitle, artistName, releaseYear, label);
 
-        let globalArrayPosition = artistsContainsName(releaseArtistName);
+        //find the position of the
+        let globalArrayPosition = labelsContainsName(releaseLabelName);
 
-        if (globalArrayPosition !== -1) {
+        if (globalArrayPosition === null) {
 
-            //Get the artist from the global array
-            let theArtist = globalArtists[globalArrayPosition];
+          //Create new label with new label-array and add artist to the global array
+          globalLabels.push(new Label(releaseLabel, new Array(theRelease)));
+          totalReleases += 1;
+          totalLabels += 1;
 
-            //Add this release to the artist's releases, if it's not in the array already
-            if (!releasesContainsTitle(theArtist.releases, theRelease.title)) {
-                theArtist.releases.push(theRelease);
-                totalReleases += 1;
-            }
 
         } else {
+            //Access label from the global array
+            let theLabel = globalLabels[globalArrayPosition];
 
-            //Create new artist with new release-array and add artist to the global array
-            globalArtists.push(new artist(releaseArtistName, new Array(thisRelease)));
-            totalReleases += 1;
-            totalArtists += 1;
-
+            //Add this release to the label's releases
+                theLabel.releases.push(theRelease);
+                totalReleases += 1;
         }
-
     });
-}
+};
 
 /************************************
 //                                  *
