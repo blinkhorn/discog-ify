0e82f949ef2bbab7b5628d9f5e9edd64778b56e8
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2017-10-27 10:28:11 -0400

    added functionality to js

--- js/select.js
+++ js/select.js
@@ -9,6 +9,13 @@ let globalArtists = [];
 let globalLabels = [];
 let totalReleases = 0;
 let totalLabels = 0;
+let playlistID = null;
+let multipleMatches;
+let withoutMatches;
+let addedCount = 0;
+let adedArtistCount = 0;
+
+let userNameDiscogs;
 
 /************************************
 //                                  *
@@ -24,12 +31,13 @@ class Release {
   }
 }
 
-class Artist {
-  constructor(name, releases) {
-    this.name = name;
-    this.releases = releases;
-  }
-}
+// ************WILL IMPLEMENT AFTER MVP COMPLETE *********************
+// class Artist {
+//   constructor(name, releases) {
+//     this.name = name;
+//     this.releases = releases;
+//   }
+// }
 
 class Label {
   constructor(name, releases) {
@@ -55,69 +63,73 @@ function getURLParams() {
   return urlParams;
 };
 
+const params = getURLParams();
+spotify_token = params.access_token;
+console.log('spotify token:', spotify_token)
+
+// ************WILL IMPLEMENT AFTER MVP COMPLETE *********************
 //prevents duplicate artists from being in the global array. If the array contains
 //the artist's name, it returns the position in the array; otherwise it returns null
-const containsName = function artistsContainsName(name) {
-    for (var i = 0; i < globalArtists.length; i += 1) {
-        if (globalArtists[i].name === name) {
-            return i;
-        }
-    }
-    return null;
-};
+// const containsName = function artistsContainsName(name) {
+//   for (var i = 0; i < globalArtists.length; i += 1) {
+//     if (globalArtists[i].name === name) {
+//       return i;
+//     }
+//   }
+//   return null;
+// };
 
 //prevents duplicate labels from being in the global array. If the array contains
 //the label's name, it returns the position in the array; otherwise it returns null
 const containsLabel = function labelsContainsName(name) {
-    for (var i = 0; i < globalLabels.length; i += 1) {
-        if (globalLabels[i].name === name) {
-            return i;
-        }
+  for (var i = 0; i < globalLabels.length; i += 1) {
+    if (globalLabels[i].name === name) {
+      return i;
     }
-    return null;
+  }
+  return null;
 };
 
 //takes the result returned from accessing all label releases from discogs and
 //adds them to the global array (duplicate releases aren't allowed)
 const aLReleases = function addLabelReleases(discogsResult) {
 
-    $.each(discogsResult.releases, (release) => {
+  $.each(discogsResult.releases, (release) => {
 
-        let releaseTitle = release.title;
-        let releaseYear = release.year;
-        let releaseArtists = release.artists;
-        let releaseArtistName = releaseArtists[0].name;
-        let releaseLabelName = release.label;
+    let releaseTitle = release.title;
+    let releaseYear = release.year;
+    let releaseArtists = release.artists;
+    let releaseArtistName = releaseArtists[0].name;
+    let releaseLabelName = release.label;
 
-        //Some artists/labels on Discogs have a number in closing round
-        //parenthesis behind their name — we prevent these here
-        let splitName = releaseArtistName.split(/([(]\d+[)].*)/);
-        let artistName = splitName[0];
-        let splitLabel = releaseLabelName.split(/([(]\d+[)].*)/);
-        let releaseLabel = splitLabel[0];
+    //Some artists/labels on Discogs have a number in closing round
+    //parenthesis behind their name — we prevent these here
+    let splitName = releaseArtistName.split(/([(]\d+[)].*)/);
+    let artistName = splitName[0];
+    let splitLabel = releaseLabelName.split(/([(]\d+[)].*)/);
+    let releaseLabel = splitLabel[0];
 
-        let theRelease = new Release(releaseTitle, artistName, releaseYear, label);
+    let theRelease = new Release(releaseTitle, artistName, releaseYear, label);
 
-        //find the position of the
-        let globalArrayPosition = labelsContainsName(releaseLabelName);
+    //find the position of the
+    let globalArrayPosition = labelsContainsName(releaseLabelName);
 
-        if (globalArrayPosition === null) {
+    if (globalArrayPosition === null) {
 
-          //Create new label with new label-array and add artist to the global array
-          globalLabels.push(new Label(releaseLabel, new Array(theRelease)));
-          totalReleases += 1;
-          totalLabels += 1;
+      //Create new label with new label-array and add artist to the global array
+      globalLabels.push(new Label(releaseLabel, new Array(theRelease)));
+      totalReleases += 1;
+      totalLabels += 1;
 
+    } else {
+      //Access label from the global array
+      let theLabel = globalLabels[globalArrayPosition];
 
-        } else {
-            //Access label from the global array
-            let theLabel = globalLabels[globalArrayPosition];
-
-            //Add this release to the label's releases
-                theLabel.releases.push(theRelease);
-                totalReleases += 1;
-        }
-    });
+      //Add this release to the label's releases
+      theLabel.releases.push(theRelease);
+      totalReleases += 1;
+    }
+  });
 };
 
 /************************************
@@ -128,7 +140,108 @@ const aLReleases = function addLabelReleases(discogsResult) {
 
 $(document).ready(() => {
 
+  $('.generate-playlist').hover(function () {
+       $(this).css('cursor', 'pointer');
+   });
+
   const params = getURLParams();
   spotify_token = params.access_token;
+  //Set exportIsActive to false on page load in the event that the previous
+  //export did not complete
+  exportIsActive = false;
 
-});
+  ///////////REWORK /////////
+  // Check the login state; set usrID, usrCountry, and usrNameSpotify
+  if (spotify_token) {
+    $.ajax({
+      url: 'https://api.spotify.com/v1/me',
+      headers: {
+        'Authorization': 'Bearer ' + spotify_token
+      },
+      success: (response) => {
+
+        // $('#login').hide();
+        // $('#loggedin').show();
+
+        usrID = response.id;
+        usrCountry = response.country;
+        usrNameSpotify = response.display_name;
+        usrImageURL = '';
+        usrImage = '';
+
+        if (response.images[0] != null) {
+          usrImageURL = response.images[0].url;
+        }
+
+        if (usrImageURL !== '') {
+          usrImage = '<img src=""' + usrImageURL + '>'
+        }
+
+        ////BRING BACK vvvvv
+
+        // if (usrNameSpotify === null) {
+        //   $('#loggedin').html(usrImage + '<p> Spotify User: ' + usrID + '</p>');
+        // } else {
+        //   $('#loggedin').html(usrImage + '<p> Spotify User: ' + usrNameSpotify + '</p>');
+        // }
+
+      },
+      error: (xhr, data) => {
+        window.location = 'file:///Users/Tiptoe/wdi/projects/discog-ify/discog-ify.html';
+      }
+    });
+  } else {
+    window.location = 'file:///Users/Tiptoe/wdi/projects/discog-ify/discog-ify.html';
+  }
+
+  // Start-Button
+  $('.generate-playlist').click(function() {
+
+    //Prevent starting the export twice
+    if (exportActive == true) {
+      return;
+    } else(exportActive = true);
+
+      //Reset some of the global values when the start-button is clicked
+      globalArtists = [];
+      playlistID = null;
+      multipleMatches = [];
+      withoutMatches = [];
+      addedCount = 0;
+      totalReleases = 0;
+      adedArtistCount = 0;
+
+      labelNameDiscogs = $('.generate-playlist-input').val();
+
+      $('#imageDiv').empty();
+      // $('#progressDiv').removeClass('hide');
+      // updateProgressBar(0);
+
+      //Start after a timeout so the Browser has time to display the changes
+      setTimeout(getCollection, 10, userNameDiscogs, 1);
+    });
+
+    $('.generate-playlist').hover(function() {
+      $(this).css('cursor', 'pointer');
+    });
+
+    // Make the user choose the right release
+    $('#releasesAdded').on('hidden.bs.modal', function(e) {
+      exportMultipleMatches();
+    });
+
+    // And again after the modal has been hidden
+    $('#bestMatch').on('hidden.bs.modal', function(e) {
+      exportMultipleMatches();
+    });
+
+    // Create Playlist
+    $('#collectionFetched').on('hidden.bs.modal', function(e) {
+      createPlaylist();
+    });
+
+    // Set the progress bar to 100% in the end
+    $('#noMatch').on('hidden.bs.modal', function(e) {
+      updateProgressBar(100);
+    });
+  });
