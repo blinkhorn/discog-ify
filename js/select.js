792e0b2176d3f0c7e16690b03c061551258c9da8
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2018-02-24 14:52:50 -0500

    added window to hopefully fix the setTimeout

--- js/select.js
+++ js/select.js
@@ -141,7 +141,7 @@ function identifyLabelResults(discogsResult) {
     if (resultType === 'release') {
       //searches for the result on discogs using its ID if it's a release
       // window.setTimeout(searchReleaseDiscogs, 1000, resultID, resultTitle);
-      setTimeout(function() {
+      window.setTimeout(function() {
         searchReleaseDiscogs(resultID, resultTitle);
       }, 1000);
     }
