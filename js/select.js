8787976fc32717f7ddab8168f615412ce89ba13d
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2018-02-24 21:20:26 -0500

    same deal, trying again

--- js/select.js
+++ js/select.js
@@ -141,7 +141,9 @@ function identifyLabelResults(discogsResult) {
     if (resultType === 'release') {
       //searches for the result on discogs using its ID if it's a release
       // window.setTimeout(searchReleaseDiscogs, 1000, resultID, resultTitle);
-      setTimeout(searchReleaseDiscogs(resultID, resultTitle), 1000);
+      setTimeout(function() {
+        searchReleaseDiscogs(resultID, resultTitle);
+      }, 1000);
     }
   });
 }
